5c84eac77468f604ffbc98f1ac0b4c1de197a0b8
Vandenhoven Nokah <vandenhoven_nokah@hotmail.com>
2014-07-27 11:36:04 +0200

    Update connect.php
    password securing

--- core/database/connect.php
+++ core/database/connect.php
@@ -2,7 +2,7 @@
 
 $connect_error = "sorry, we\'re experiening connection problems.";
 
-mysql_connect('localhost', 'root', '8se4738P') or die($connect_error);
+mysql_connect('xxxxxxxxxx', 'xxxxxxxxxxxx', 'xxxxxxxxxxxx') or die($connect_error);
 mysql_select_db('loginandregister') or die($connect_error);
 
 ?>
